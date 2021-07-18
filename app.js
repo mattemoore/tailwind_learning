32705a7d375b35279b34d7d49298008f9bade4a2
Matt Moore <mattmoore@pop-os.localdomain>
2021-07-18 16:53:32 -0400

    init templating

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var createError = require('http-errors');
+
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var path = require('path');
+var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -9,15 +11,14 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.engine('handlebars', exphbs());
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'handlebars');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
